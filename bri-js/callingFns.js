8f92d742ae2241d06c47c47b455386e45bd0aa7b
aidanpcole <90789077+aidanpcole@users.noreply.github.com>
2023-04-24 17:44:48 -0400

    delete raw data, new pngs

--- bri-js/callingFns.js
+++ bri-js/callingFns.js
@@ -2,7 +2,7 @@
 /* 1. === Setting up Map === */
 /* set up with zoom 5, may change, changed lat
 and long from 34,0836417742618, -118.5298649280784 */
-let map = L.map('map', { zoomControl: false }).setView([21.196640407274728, 72.40726019546284], 5);
+let map = L.map('map', { zoomControl: false }).setView([22, 78.5], 5);
 
 const basemap = 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png';
 const attribution = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.';
